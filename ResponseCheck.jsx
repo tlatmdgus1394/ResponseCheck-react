072582854989113fb426b605616ac2a830988581
SeungHyun Shim <tlatmdgus1394@gmail.com>
2022-03-29 02:07:59 +0900

    class를 사용한 코드

---
+++ ResponseCheck.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+class ResponseCheck extends Component {
+  state = {
+    state: "waiting",
+    message: "클릭해서 시작하세요.",
+    result: [],
+  };
+
+  timeout;
+  startTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === "waiting") {
+      this.setState({
+        state: "ready",
+        message: "초록색이 되면 클릭하세요.",
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: "now",
+          message: "지금 클릭하세요!",
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === "ready") {
+      clearTimeout(this.timeout);
+      this.setState({
+        state: "waiting",
+        message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요.",
+      });
+    } else if (state === "now") {
+      this.setState((prevState) => {
+        return {
+          state: "waiting",
+          message: "클릭해서 시작하세요.",
+          result: [...prevState.result, new Date() - this.startTime],
+        };
+      });
+    }
+  };
+
+  render() {
+    const { state, result, message } = this.state;
+    return (
+      <>
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
+        </div>
+        {result.length === 0 ? null : (
+          <div>
+            평균 시간: {result.reduce((a, c) => a + c) / result.length}
+            ms
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default ResponseCheck;
---
+++ client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NumberBaseball from "./ResponseCheck";
+
+ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
